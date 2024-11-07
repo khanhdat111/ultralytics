d32117680bdf92ed2f9df81c5f52f1f979b05d5a
Clozer (Khanh Dat) <128824280+khanhdat111@users.noreply.github.com>
2024-11-07 14:51:24 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1042,6 +1042,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 C2fPSA,
                 C2fCIB,
                 C2PSA,
+                CBAM
             }:
                 args.insert(2, n)  # number of repeats
                 n = 1
