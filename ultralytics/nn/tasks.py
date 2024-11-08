e362800be869177f4b7dcc5576c72de9533c5645
Clozer (Khanh Dat) <128824280+khanhdat111@users.noreply.github.com>
2024-11-08 13:22:02 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1077,14 +1077,10 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
         elif m is CBFuse:
             c2 = ch[f[-1]]
         elif m is CBAM:
-            c1 = ch[f]
-            if len(args) == 0:
-                args = [c1]
-            elif len(args) == 1:
-                args = [c1, args[0]]
-            else:
-                raise ValueError(f"CBAM expected at most 1 argument (kernel_size), got {len(args)}")
-            c2 = c1  # CBAM không thay đổi số kênh
+            c1, c2 = ch[f], args[0]
+            if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+                c2 = make_divisible(min(c2, max_channels) * width, 8)
+            args = [c1,*args[1:]]
         else:
             c2 = ch[f]
 
