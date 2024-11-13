d266dfde90addd9edae323821ddc86a0e3355e6a
Clozer (Khanh Dat) <128824280+khanhdat111@users.noreply.github.com>
2024-11-13 10:34:45 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -972,7 +972,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
     if verbose:
         LOGGER.info(f"\n{'':>3}{'from':>20}{'n':>3}{'params':>10}  {'module':<45}{'arguments':<30}")
     ch = [ch]
+    print(f"ch: {ch}")
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
+    print(f"c2: {c2} ")
     for i, (f, n, m, args) in enumerate(d["backbone"] + d["head"]):  # from, number, module, args
         m = getattr(torch.nn, m[3:]) if "nn." in m else globals()[m]  # get module
         for j, a in enumerate(args):
