89edb77fa4a8108c75f3aad1c86e0ae33868315e
Clozer (Khanh Dat) <128824280+khanhdat111@users.noreply.github.com>
2024-11-21 23:59:08 +0700

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -23,8 +23,28 @@ __all__ = (
     "Concat",
     "RepConv",
     "Addition",
-    "ResBlock_CBAM"
+    "ResBlock_CBAM",
+    "se_block"
 )
+
+class se_block(nn.Module):   #SEnet
+    def __init__(self, channel,  ratio=16):
+        super(se_block, self).__init__()
+        channel = channel//2
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // ratio, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // ratio, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        # b,c,h,w -> b,c,1,1,
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
     """Pad to 'same' shape outputs."""
