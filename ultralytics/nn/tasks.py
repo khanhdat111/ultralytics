005c775ff26d381b4699a47dc7ffc76ce7319215
Clozer (Khanh Dat) <128824280+khanhdat111@users.noreply.github.com>
2024-11-21 09:47:22 +0700

    Update tasks.py

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -78,7 +78,8 @@ from ultralytics.nn.modules import (
     Segment,
     WorldDetect,
     v10Detect,
-    Input
+    Input,
+    ResBlock_CBAM
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
